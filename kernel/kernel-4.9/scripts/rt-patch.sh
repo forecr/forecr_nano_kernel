2975fa23da85a76c7f92e7cda41da254f4fa6a31
mdeniz0 <mehmetdeniz@mistelektronik.com>
2021-12-24 16:44:52 +0300

    rt-patch.sh updated

--- kernel/kernel-4.9/scripts/rt-patch.sh
+++ kernel/kernel-4.9/scripts/rt-patch.sh
@@ -11,7 +11,7 @@
 any_failure=0
 apply_rt_patches()
 {
-	count=$(ls ../arch/arm64/configs/.tmp.tegra*defconfig 2>/dev/null| wc -l)
+	count=$(ls ../arch/arm64/configs/.tmp.*defconfig 2>/dev/null| wc -l)
 	if [ $count -gt 0 ]; then
 		echo "The PREEMPT RT patches are already applied to the kernel!"
 	else
@@ -44,12 +44,24 @@ apply_rt_patches()
 			--disable CPU_FREQ_GOV_SCHEDUTIL \
 			--disable CPU_FREQ_GOV_INTERACTIVE || any_failure=1
 		echo "PREEMPT RT patches successfully applied for L4T!"
+
+		#make temporary copy of the Forecr's defconfig file
+		cp ../arch/arm64/configs/dsboard_nx2_nano_defconfig\
+			../arch/arm64/configs/.tmp.dsboard_nx2_nano_defconfig
+		 ./config --file ../arch/arm64/configs/dsboard_nx2_nano_defconfig\
+			--enable PREEMPT_RT_FULL \
+			--disable DEBUG_PREEMPT \
+			--disable CPU_IDLE_TEGRA18X \
+			--disable CPU_FREQ_TIMES \
+			--disable CPU_FREQ_GOV_SCHEDUTIL \
+			--disable CPU_FREQ_GOV_INTERACTIVE || any_failure=1
+		echo "PREEMPT RT patches successfully applied for DSBOARD_NX2!"
 	fi
 }
 
 revert_rt_patches()
 {
-	count=$(ls ../arch/arm64/configs/.tmp.tegra*defconfig 2>/dev/null| wc -l)
+	count=$(ls ../arch/arm64/configs/.tmp.*defconfig 2>/dev/null| wc -l)
 	if [ $count -gt 0 ]; then
 		file_list=`find ../rt-patches -name \*.patch -type f | sort -r`
 		for p in $file_list; do
@@ -63,6 +75,10 @@ revert_rt_patches()
 		cp ../arch/arm64/configs/.tmp.tegra_defconfig\
 			../arch/arm64/configs/tegra_defconfig
 		rm -rf ../arch/arm64/configs/.tmp.tegra_defconfig
+
+		cp ../arch/arm64/configs/.tmp.dsboard_nx2_nano_defconfig\
+			../arch/arm64/configs/dsboard_nx2_nano_defconfig
+		rm -rf ../arch/arm64/configs/.tmp.dsboard_nx2_nano_defconfig
 		echo "The PREEMPT RT patches have been successfully reverted!"
 	else
 		echo "The PREEMPT RT patches are not applied to the kernel!"
